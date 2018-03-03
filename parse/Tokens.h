4e3d6badb0f92f4e394de5b55fd24f99347abdac
geoffthemedio <geoffthemedio@gmail.com>
2018-03-03 14:59:40 +0100

    Parsing WIP

--- parse/Tokens.h
+++ parse/Tokens.h
@@ -543,6 +543,7 @@
     (Troops)                                    \
     (Tundra)                                    \
     (Turn)                                      \
+    (TurnPolicyAdopted)                         \
     (TurnsSinceFocusChange)                     \
     (TurnTechResearched)                        \
     (Type)                                      \
