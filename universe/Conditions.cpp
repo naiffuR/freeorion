f420aeedda619ba3f88d267f035f6f6abbaa42fa
geoffthemedio <geoffthemedio@gmail.com>
2018-03-17 16:41:15 +0100

    Policy tracking and adoption reworking.

--- universe/Conditions.cpp
+++ universe/Conditions.cpp
@@ -6675,7 +6675,7 @@ namespace {
             if (!empire)
                 return false;
 
-            return empire->AdoptedPolicyTurns().count(m_name);
+            return empire->PolicyAdopted(m_name);
         }
 
         std::string m_name;
